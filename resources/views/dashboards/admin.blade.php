4a3de9641c92d884d5634dcdc6f45d30925e4483
amir <amir_kindboy@yahoo.com>
2023-09-09 20:21:21 +0430

    changing config file

---
+++ resources/views/dashboards/admin.blade.php
@@ -0,0 +1,57 @@
+@extends('layouts.app')
+
+@section('content')
+<h2 class="dinar text-center text-primary"> نقشه سایت</h2>
+<hr>
+
+<div class="text-center p-3">
+    <div class="card my-2">
+        <div class="card-body">
+             <span class="lead">مدیریت منو</span>
+        </div>
+        <div class="card-footer text-left">
+             <a class="text-decoration-none mx-1 text-success" href=""><i class="ti-pencil s-1-5x" title="ویرایش"></i></a>
+                @if (true)
+                <a class="text-decoration-none mx-1 text-warning" href=""><i class="ti-na s-1-5x" title="عدم نمایش"></i></a>
+
+                @else
+                <a class="text-decoration-none mx-1 text-warning" href=""><i class="ti-eye s-1-5x" title=" نمایش"></i></a>
+
+                @endif 
+            
+        </div>
+     </div>
+     <div class="card my-2">
+          <div class="card-body">
+               <span class="lead">مدیریت هدر</span>
+          </div>
+          <div class="card-footer text-left">
+               <a class="text-decoration-none mx-1 text-success" href=""><i class="ti-pencil s-1-5x" title="ویرایش"></i></a>
+                  @if (true)
+                  <a class="text-decoration-none mx-1 text-warning" href=""><i class="ti-na s-1-5x" title="عدم نمایش"></i></a>
+  
+                  @else
+                  <a class="text-decoration-none mx-1 text-warning" href=""><i class="ti-eye s-1-5x" title=" نمایش"></i></a>
+  
+                  @endif 
+              
+          </div>
+       </div>
+       <div class="card my-2">
+          <div class="card-body">
+               <span class="lead">مدیریت  فوتر</span>
+          </div>
+          <div class="card-footer text-left">
+               <a class="text-decoration-none mx-1 text-success" href=""><i class="ti-pencil s-1-5x" title="ویرایش"></i></a>
+                  @if (true)
+                  <a class="text-decoration-none mx-1 text-warning" href=""><i class="ti-na s-1-5x" title="عدم نمایش"></i></a>
+  
+                  @else
+                  <a class="text-decoration-none mx-1 text-warning" href=""><i class="ti-eye s-1-5x" title=" نمایش"></i></a>
+  
+                  @endif 
+              
+          </div>
+       </div>
+</div>
+@endsection
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'ECMS') }}</title>
+      <!-- Fonts -->
+      <link href="{{ asset('css/fonts.css') }}" rel="stylesheet">
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/themify-icons.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/panel.css') }}" rel="stylesheet">
+
+</head>
+<body>
+    <div id="app">
+       @include('fragments.nav')
+
+        <main class="py-4">
+            @yield('content')
+        </main>
+    </div>
+     <!-- Scripts -->
+     <script src="{{ asset('js/app.js') }}"></script>
+</body>
+</html>
